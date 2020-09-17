3a8090021ab59446dc796c0655c1a562410d7627
ico4e <70281513+ico4e@users.noreply.github.com>
2020-09-17 11:53:10 -0400

    v0.0.2

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hi salva');
-});
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-scaffold', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log('Server on port', PORT);
+});
